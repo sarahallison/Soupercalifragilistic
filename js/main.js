ae3b1d5f36376356f7fbd854c0d5b67787b74dc4
sarahallison <Sarah.rides.15@gmail.com>
2017-02-22 15:56:19 -0500

    6commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('footer a').click(function(event){
+  
+  // prevent defalt click behavior
+  // don't jump to content
+  event.preventDefault();
+    
+  // identify position of target
+  var target = $(this).attr('href');
+  var top = $(target).offset().top;
+
+  // animate scroll to target
+  $('html,body').animate({
+    scrollTop: top
+    },2000);
+
+});
